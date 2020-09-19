25266c81cd5f21b1d8cd03d0c99656848f223e21
Prateek Nayan <prateek.nayan123@gmail.com>
2020-09-19 02:14:39 -0500

    Updates

---
+++ precache-manifest.aa0ff1d4944a7f7f42979d12f640f111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2f3ab601e1a36904779b4f565a63bb",
+    "url": "/Porfolio/index.html"
+  },
+  {
+    "revision": "6c19c48d7b4ac9dab99a",
+    "url": "/Porfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a9021bb05bbc4a99eed8",
+    "url": "/Porfolio/static/js/2.48b8d7f0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Porfolio/static/js/2.48b8d7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c19c48d7b4ac9dab99a",
+    "url": "/Porfolio/static/js/main.b84f91cc.chunk.js"
+  },
+  {
+    "revision": "0bdc56a7c28ca5c7abe4",
+    "url": "/Porfolio/static/js/runtime-main.2da23313.js"
+  }
+]);
